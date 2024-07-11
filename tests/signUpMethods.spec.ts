9304bdde8001567e3c343c0a258ad97aea6531a4
TypeScriptFox <jb_dev@icloud.com>
2024-07-11 17:00:58 -0500

    test

--- tests/signUpMethods.spec.ts
+++ tests/signUpMethods.spec.ts
@@ -1,7 +1,7 @@
 import endpoint from "./configTypes"
 import { test, expect } from '@playwright/test'
 
-test("Expect to have 3 options for signing up", async ({ page }) => {
+test("Expect to have 0 options for signing up", async ({ page }) => {
 
   // Go to the Droplets product page of DigitalOcean web page
   await page.goto(endpoint.DIGITAL_OCEAN_URL);
